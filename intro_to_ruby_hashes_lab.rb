e94765bc6269df1f90802c849a7ed35d392667e5
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-09-26 20:55:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,10 +12,16 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+    airports_in_usa => {
+    :new_york => {
+      :air1 => "JFK",
+      :air2 => "LaGuardia"
+    },
+    :athens => {
+      :air1 => "Eleftherios Venizelos",
+      :air2 => "Hellinikon"
+    }
+  }
 end
 
 def monopoly_with_third_tier
