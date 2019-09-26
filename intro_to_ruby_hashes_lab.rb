1b16962e16221ee5613d0b8082438a832831dddd
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-09-26 21:02:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,10 +31,20 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+airports_in_usa => {
+    :new_york => {
+      :air1 => "JFK",
+      :air2 => "LaGuardia"
+    },
+    airports_in_greece => {
+    :athens => {
+      :air1 => "Eleftherios Venizelos",
+      :air2 => "Hellinikon"
+    },
+    airports = {
+      :pieces => ,
+      :rent_in_dollars =>
+      
 end
 
 def monopoly_with_fourth_tier
